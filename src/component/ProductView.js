d3ac67a38f0be9fea3562ff89fa4513ff71d79a6
Shane Wolff <wolffshane@gmail.com>
2020-09-16 11:57:21 +0530

    Added a progress bar to indicate the current total stock level occupied.

--- src/component/ProductView.js
+++ src/component/ProductView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
@@ -7,6 +7,12 @@ import { PencilFill, TrashFill, PlusCircleFill } from 'react-bootstrap-icons';
 import { deleteProduct } from '../service/InventoryService';
 
 const ProductView = (props) => {
+
+	const [totalStockLevel, setTotalStockLevel] = useState(0);
+
+	useEffect(function updateTotalStockLevel() {
+		setTotalStockLevel(props.products.reduce((total, current) => total + current.stockLevel, 0));
+	}, [props.products]);
 
 	const handleDelete = id => {
 
@@ -34,6 +40,21 @@ const ProductView = (props) => {
 		</tr>
 	);
 
+	const getProgressBarClasses = () => {
+		const progressBarClasses = ['progress-bar', 'progress-bar-striped', 'progress-bar-animated'];
+		if (totalStockLevel <= 25) {
+			progressBarClasses.push('bg-success');
+		} else if (totalStockLevel <= 50) {
+			progressBarClasses.push('bg-info');
+		} else if (totalStockLevel <= 75) {
+			progressBarClasses.push('bg-warning');
+		} else if (totalStockLevel > 75) {
+			progressBarClasses.push('bg-warning');
+		}
+
+		return progressBarClasses.join(' ');
+	};
+
 	return (
 		<React.Fragment>
 			<Row>
@@ -42,6 +63,14 @@ const ProductView = (props) => {
 					<br />
 				</Col>
 			</Row>
+			<Row>
+				<Col>
+					<div className="progress" style={{ height: '2.5rem' }}>
+						<div className={getProgressBarClasses()} role="progressbar" aria-valuenow={totalStockLevel} aria-valuemin="0" aria-valuemax="100" style={{ width: `${totalStockLevel}%` }}><strong style={{fontSize: '1rem'}}>CURRENT STOCK LEVEL: {totalStockLevel}%</strong></div>
+					</div>
+				</Col>
+			</Row>
+			<br />
 			<Row>
 				<Col>
 					<Table striped hover>
